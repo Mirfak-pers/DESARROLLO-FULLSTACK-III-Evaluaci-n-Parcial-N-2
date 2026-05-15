6ea3fdc5bdfc896c43eca281b6b3b62f61e0ead3
Gabriel Hernandez <gabrielgamer498@gmail.com>
2026-05-15 14:48:49 -0400

    Swagger

--- Backend/smartlogix-parent/Usuarios/src/main/java/com/smartlogix/msUsuarios/security/SecurityConfig.java
+++ Backend/smartlogix-parent/Usuarios/src/main/java/com/smartlogix/msUsuarios/security/SecurityConfig.java
@@ -22,7 +22,8 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/usuarios/registro", "/api/usuarios/login").permitAll()
+                .requestMatchers("/api/usuarios/registro", "/api/usuarios/login","/swagger-ui.html", "/swagger-ui/**",
+"/v3/api-docs", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
             );
         return http.build();
